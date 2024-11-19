3bf5c4f893471bb5b99f2bcdd62d700efeb87e98
Nmugol <44527408+Nmugol@users.noreply.github.com>
2024-11-19 11:34:32 +0100

    Ograniczenie wyświtlani do 2 miejsc po przeccinku

--- jadlospis/ViewModels/ProduktViewModel.cs
+++ jadlospis/ViewModels/ProduktViewModel.cs
@@ -54,14 +54,14 @@ public partial class ProduktViewModel : ViewModelBase
     {
         // Inicjalizacja właściwości na podstawie obiektu produktu
         Name = string.IsNullOrWhiteSpace(product.Name) ? "Brak nazwy" : product.Name;
-        Carbs = product.Nutriments.Carbs;
-        Sugar = product.Nutriments.Sugar;
-        Energy = product.Nutriments.Energy;
-        EnergyKcal = product.Nutriments.EnergyKcal;
-        Fat = product.Nutriments.Fat;
-        SaturatedFat = product.Nutriments.SaturatedFat;
-        Protein = product.Nutriments.Protein;
-        Salt = product.Nutriments.Salt;
+        Carbs = Math.Round(product.Nutriments.Carbs, 2);
+        Sugar = Math.Round(product.Nutriments.Sugar, 2);
+        Energy = Math.Round(product.Nutriments.Energy, 2);
+        EnergyKcal = Math.Round(product.Nutriments.EnergyKcal, 2);
+        Fat = Math.Round(product.Nutriments.Fat, 2);
+        SaturatedFat = Math.Round(product.Nutriments.SaturatedFat, 2);
+        Protein = Math.Round(product.Nutriments.Protein, 2);
+        Salt = Math.Round(product.Nutriments.Salt, 2);
         image = product.ImageUrl;
 
         // Ładowanie obrazu na podstawie przekazanej ścieżki URL
